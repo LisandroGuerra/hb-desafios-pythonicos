f87f3c8b9c3071a27b1b3a4c2bda4c76c8bc4366
LisandroGuerra <lisandro.digital@gmail.com>
2020-05-13 10:26:58 -0300

    Atualiza 06

--- 06_not_bad.py
+++ 06_not_bad.py
@@ -10,8 +10,17 @@ Exemplo: 'The dinner is not that bad!' retorna 'The dinner is good!'
 """
 
 def not_bad(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    splited = s.split()
+    p = ''
+    if splited[-1][-1] in ':.;!?':
+        p = splited[-1][-1]
+        splited[-1] = splited[-1][:-1]
+    if 'bad' in splited and 'not' in splited:
+        n_index = splited.index('not')
+        b_index = splited.index('bad')
+        if n_index < b_index:
+            s = " ".join(splited[:n_index] + ['good'] + splited[b_index+1:]) + p
+    return s
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
@@ -39,3 +48,5 @@ if __name__ == '__main__':
     test(not_bad, 'This dinner is not that bad!', 'This dinner is good!')
     test(not_bad, 'This tea is not hot', 'This tea is not hot')
     test(not_bad, "It's bad yet not", "It's bad yet not")
+    test(not_bad, 'Is this movie not so bad?', 'Is this movie good?')
+    test(not_bad, "It's can be bad, but is not so bad", "It's can be bad, but is good")
