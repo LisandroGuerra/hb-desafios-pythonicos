171a4f6fd42b60164a74d808f6c3b29caf926849
LisandroGuerra <lisandro.digital@gmail.com>
2020-11-25 02:09:01 -0300

    13_wordcount.py v1

--- 13_wordcount.py
+++ 13_wordcount.py
@@ -70,9 +70,27 @@ def print_words(filename):
         print(word, freq)
 
 
-
 def print_top(filename):
-    pass
+    with open(filename) as file:
+        text = file.read().lower().split()
+
+    words_dict = {}
+    for word in text:
+        if word in words_dict:
+            words_dict[word] += 1
+        else:
+            words_dict[word] = 1
+
+    top_dict = {}
+    for word, freq in words_dict.items():
+        top_dict[freq] = word
+
+    top_list = list(sorted(top_dict.items(), reverse=True))[:20]
+    for element in top_list:
+        print(f'{element[1]} {element[0]}')
+
+    # for freq, word in sorted(top_dict.items(), reverse=True):
+    #     print(freq, word)
 
 # A função abaixo chama print_words() ou print_top() de acordo com os
 # parêtros do programa.
