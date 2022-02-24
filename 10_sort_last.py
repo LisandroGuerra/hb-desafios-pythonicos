cfd1c2ceffdce59fc897a9d31404fc8efb61504a
Lisandro Guerra <lisandro.digital@gmail.com>
2022-02-24 20:42:47 -0300

    New version to 10 - sort last - using lambda

--- 10_sort_last.py
+++ 10_sort_last.py
@@ -9,10 +9,13 @@ Irá retornar: [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 
 Dica: Use uma custom key= function para extrair o ultimo elemento de cada tupla.
 """
+# def sort_last(tuples):
+#     def take_last(element):
+#         return element[-1]
+#     return sorted(tuples,  key=take_last)
+
 def sort_last(tuples):
-    def take_last(element):
-        return element[-1]
-    return sorted(tuples,  key=take_last)
+    return sorted(tuples,  key=lambda le: le[-1])
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
